3908bf05e0e74f9d68e993eed19426fe4c96279a
tung-hpzbook <tung-hpzbook@staas.io>
2024-05-04 21:58:00 +0700

    1.0.10 add GSheet

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-try { fs.existsSync(__dirname + '/.env') && require('dotenv').config() && console.log('Success: env loaded ' + __dirname + '/.env'); } catch {}
-
 const fs = require('fs');
 const path = require('path');
+
+const dotenv =__dirname + '/.env';// console.log(dotenv)
+try { fs.existsSync(dotenv) && require('dotenv').config() && console.log(' > SUCCESS: Dotenv is loaded ' + dotenv); } catch {}
 
 const YAML = require('yaml');
 const { program } = require('commander');
@@ -16,37 +17,35 @@ program
 	.option('-s, --source <source>', 'Config source: mongo collection name/gsheet name/redis key/git repo/s3 path')
 	.option('-q, --query <query>', 'Config query')
 	.option('-i, --init', 'init')
+	.parse(process.argv);
 
-program.parse(process.argv);
-const PARAMS = program.opts();// console.dir(PARAMS);
-const CONFIG_DIR = path.join(PARAMS.dir);
+const { from, source, dir, init } = program.opts();
 
-!fs.existsSync(CONFIG_DIR) && fs.mkdirSync(CONFIG_DIR, { recursive: true });
+!fs.existsSync(dir) && fs.mkdirSync(dir, { recursive: true });
 
 const exit = (c=0, ...m) => (m?.[0] && console.log(...m)) & process.exit(c);
 
 ;(async function main() {
-	let {from} = PARAMS;
-
 	try {
 		if (from.includes('mongo')) await configMongo();
+		if (from.match(/sheets?\//i)) await configGSheet();
 	} catch (ex) {
 		console.error(ex)
 	} finally {
-		console.log('Success: config loaded ' + from)
+		console.log(` > SUCCESS: Configs are ${init ? 'initialzed' : 'loaded'} from: ` + from)
 		exit(0);
 	}
 })();
 
 async function configMongo() {
 	const { MongoClient } = require('mongodb');
-	const client = new MongoClient(ENV.MONGO_URL || PARAMS.from);
+	const client = new MongoClient(ENV.MONGO_URL || from);
 	await client.connect();
-  	const collection = client.db().collection(PARAMS.source);
+	const collection = client.db().collection(source);
 
-  	if (PARAMS.init) {
-  		for (let file of fs.readdirSync(CONFIG_DIR)) {
-			let filepath = path.join(CONFIG_DIR, file);
+	if (init) {
+		for (let file of fs.readdirSync(dir)) {
+			let filepath = path.join(dir, file);
 			let ext = path.extname(file);
 			let key = path.basename(file, ext);
 			let js = (ext == '.js');
@@ -54,17 +53,78 @@ async function configMongo() {
 			let value = (js || yaml) ? fs.readFileSync(filepath, 'utf8') : require(filepath);
 
 			await collection.updateOne({ key }, { date: new Date(), key, value, js, yaml}, {upsert: true});
-  		}
-  	} else {
-  		const configs = await collection.find({}).toArray();
+		}
+	} else {
+		const configs = await collection.find({}).toArray();
 
 		configs.map(({key, value, js, yaml}) => {
-			let filepath = path.join(CONFIG_DIR, key + (yaml ? '.yaml' : (js ? '.js' : '.json')));
+			let filepath = path.join(dir, key + (yaml ? '.yaml' : (js ? '.js' : '.json')));
 			fs.writeFileSync(filepath, yaml ? YAML.stringify(value) : (js ? value : JSON.stringify(value, null, 2)));
 		})
-  	}
+	}
 
-  	await client.close();
+	await client.close();
 }
 
 
+async function configGSheet() {
+	const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+	let id = from;
+	if (from.includes('http') || from.includes('/d/')) {
+		try {
+			let url = new URL(from);
+			id = url.pathname.match(/\/d\/([\w\d-]*)/)?.[1] || from;
+		} catch {}
+	}
+
+	let doc = new GoogleSpreadsheet(id);
+
+	let auth = (process.env.GOOGLE_SERVICE_ACCOUNT_FILE && fs.existsSync(process.env.GOOGLE_SERVICE_ACCOUNT_FILE))
+		? require(process.env.GOOGLE_SERVICE_ACCOUNT_FILE)
+		: {
+			"private_key": process.env.GOOGLE_PRIVATE_KEY,
+			"client_email": process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+		};
+
+	if (!auth?.private_key || !auth?.client_email) return console.error('E_GOOGLE_SERVICE_ACCOUNT_ERROR');
+
+	await doc.useServiceAccountAuth(auth);
+	let info = await doc.loadInfo();
+
+	if (init) {
+		for (let file of fs.readdirSync(dir)) {
+			let filepath = path.join(dir, file);
+			let ext = path.extname(file);
+			let key = path.basename(file, ext);
+			let js = (ext == '.js');
+			let items = require(filepath);
+
+			if (!Array.isArray(items)) return console.error('E_JSON_FILE_IS_NOT_ARRAY');
+
+			let sheet = doc.sheetsByIndex.find(x => x.title == key);
+			if (!sheet) sheet = await doc.addSheet({title: key});
+			await sheet.loadCells();
+
+			await sheet.loadHeaderRow();
+			let headers = sheet.headerValues;
+
+			if (!headers?.length) sheet.addRow(Object.keys(items[0]));
+
+			await sheet.addRows(items.map(item => Object.values(items)));
+		}
+	} else {
+		let sheet = doc.sheetsByIndex.find(x => x.title == source);
+		if (!sheet) return console.error('E_SHEETNAME_NOT_FOUND');
+
+		await sheet.loadCells();
+
+		let rows = await sheet.getRows();
+		let headers = sheet.headerValues;
+
+		let objs = rows.map( row => Object.fromEntries(headers.map(h => [h, row[h]])) )
+
+		let filepath = path.join(dir, source + '.json');
+		fs.writeFileSync(filepath, JSON.stringify(objs, null, 2));
+	}
+}
